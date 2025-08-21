43f38e64f0b40d7755bde1b7eed7fc09f5a22054
Zhvansky Stanislav <staszhvansky@gmail.com>
2025-08-21 10:44:27 +0300

    rename package

--- src/main/java/org/example/behavioralpatterns/command/Main.java
+++ src/main/java/org/example/behavioralpatterns/command/Main.java
@@ -5,9 +5,6 @@ import org.example.behavioralpatterns.command.Commands.*;
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.printf("SAFasf");
-        System.out.println("sdgdasg");
         System.out.println("=== 1. Undo/Redo Example ===");
         TextEditor editor = new TextEditor();
         CommandHistory history = new CommandHistory();
